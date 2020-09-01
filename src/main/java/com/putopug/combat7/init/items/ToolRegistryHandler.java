6cb663a72951df26701e60f4bf6cbd72fb492697
PutoPug <muralihrishikesh@gmail.com>
2020-09-01 09:43:32 +0530

    Testing Sword 😁^_~

--- src/main/java/com/putopug/combat7/init/items/ToolRegistryHandler.java
+++ src/main/java/com/putopug/combat7/init/items/ToolRegistryHandler.java
@@ -10,13 +10,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class ItemRegistryHandler {
+public class ToolRegistryHandler {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, combat7.MOD_ID);
 
     public static void init(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    //Items
-    public static final RegistryObject<Item> FUNKY_GEM = ITEMS.register("funky_gem", ItemBase::new);
+    //SWORDDEL
+    public static final RegistryObject<SwordItem> FUNKY_SWORD = ITEMS.register("funky_sword", () ->
+            new SwordItem(ItemTier.NETHERITE, 10000, 0.0F, new Item.Properties().group(ItemGroup.REDSTONE)));
+    public static final RegistryObject<SwordItem> FUNfgbKY_SWORD = ITEMS.register("funkydgb_sword", () ->
+            new SwordItem(ItemTier.NETHERITE, 100, 0.0F, new Item.Properties().group(ItemGroup.REDSTONE)));
 }
